673f3f16b1d89cbc9b43aff4463c2992ec527559
Tobias Fuchs <tobias.fuchs@tfox.tech>
2025-12-20 02:34:12 +0100

    add zshrc and tmux config

--- home.nix
+++ home.nix
@@ -6,6 +6,20 @@
 	home.stateVersion = "25.11";
 	programs.zsh = { 
 		enable = true;
+		initContent = builtins.readFile "${dots}/zshrc";
+	};
+
+	programs.bat.enable = true;
+	programs.eza.enable = true;
+	programs.direnv = { 
+		enable = true; 
+		enableZshIntegration = true;
+	};
+	programs.fzf.enable = true; 
+	programs.ripgrep.enable = true;
+	programs.tmux = { 
+		enable = true;
+		extraConfig = builtins.readFile "${dots}/.tmux.conf";
 	};
 
 	programs.git = { 
@@ -15,7 +29,9 @@
 		];
 	};
 
-	home.file.".config/hypr".source = "${dots}/hypr";
-	home.file.".config/waybar".source = "${dots}/waybar";
-	home.file.".zshrc".source = "${dots}/zshrc";
+
+	home.file.".p10k.zsh".source = "${dots}/.p10k.zsh";
+	xdg.configFile."hypr".source = "${dots}/hypr";
+	xdg.configFile."waybar".source = "${dots}/waybar";
+	xdg.configFile."tmux/tmux.reset.conf".source = "${dots}/.config/tmux/tmux.reset.conf";
 }
