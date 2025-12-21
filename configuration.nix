f537b0c2e39db88eba49cfd0b2ab7648a2238c72
Tobias Fuchs <tobias.fuchs@tfox.tech>
2025-12-21 05:42:29 +0100

    add protonvpn and openvpn config

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "tobi-tower";
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
+  networking.firewall.checkReversePath = false;
 
   time.timeZone = "Europe/Berlin";
 
@@ -54,10 +60,15 @@
   };
 
   services.xserver.videoDrivers = [ "nvidia" ];
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.hyprland.enableGnomeKeyring = true;
 
   users.users.tobi = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
     packages = with pkgs; [
       tree
       spotify
@@ -81,14 +92,19 @@
 
   environment.systemPackages = with pkgs; [
     cargo
-    vim
     git
-    wget
-    wezterm
-    waybar
-    wofi
     hyprpaper
     kitty
+    networkmanager-openvpn
+    networkmanagerapplet
+    proton-pass
+    protonvpn-gui
+    vim
+    waybar
+    wezterm
+    wget
+    wireguard-tools
+    wofi
   ];
 
   fonts.packages = with pkgs; [
@@ -132,23 +148,7 @@
     options = "--delete-older-than 30d";
   };
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  # never change
+  system.stateVersion = "25.11";
 
 }
--- home.nix
+++ home.nix
@@ -9,38 +9,40 @@
     nixvim.packages.x86_64-linux.default
   ];
 
-  programs.zsh = {
-    enable = true;
-    initContent = builtins.readFile "${dots}/zshrc";
-    shellAliases = {
-      rebuild-system = "sudo nixos-rebuild switch --flake ~/nixos-config";
-      upgrade-system = "sudo nixos-rebuild switch --flake ~/nixos-config --upgrade";
+  programs = {
+    bat.enable = true;
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
     };
-  };
+    discord.enable = true;
+    eza.enable = true;
+    bemenu.enable = true;
+    fzf.enable = true;
+    gcc.enable = true;
+    git = {
+      enable = true;
+      includes = [
+        { path = "~/.gitconfig.local"; }
+      ];
+    };
+    lazygit.enable = true;
+    obsidian.enable = true;
+    ripgrep.enable = true;
+    tmux = {
+      enable = true;
+      extraConfig = builtins.readFile "${dots}/.tmux.conf";
+    };
+    zsh = {
+      enable = true;
+      initContent = builtins.readFile "${dots}/zshrc";
+      shellAliases = {
+        rebuild-system = "sudo nixos-rebuild switch --flake ~/nixos-config";
+        upgrade-system = "sudo nixos-rebuild switch --flake ~/nixos-config --upgrade";
+      };
+    };
 
-  programs.bat.enable = true;
-  programs.eza.enable = true;
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
   };
-  programs.fzf.enable = true;
-  programs.ripgrep.enable = true;
-  programs.tmux = {
-    enable = true;
-    extraConfig = builtins.readFile "${dots}/.tmux.conf";
-  };
-  programs.git = {
-    enable = true;
-    includes = [
-      { path = "~/.gitconfig.local"; }
-    ];
-  };
-
-  programs.gcc.enable = true;
-  programs.obsidian.enable = true;
-  programs.lazygit.enable = true;
-  programs.discord.enable = true;
 
   home.file.".p10k.zsh".source = "${dots}/.p10k.zsh";
   xdg.configFile."hypr".source = "${dots}/hypr";
