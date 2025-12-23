8fa750af1d74c5e93d0b5abfbd46cc3542df977b
Tobias Fuchs <tobias.fuchs@tfox.tech>
2025-12-23 22:58:09 +0100

    fix obsidian dark theme

--- modules/home/obsidian/default.nix
+++ modules/home/obsidian/default.nix
@@ -5,10 +5,40 @@
     obsidian = {
       enable = true;
       defaultSettings = {
+        appearance = {
+          theme = "obsidian";
+        };
         themes = [
           {
             pkg = pkgs.callPackage ./catppuccin-theme.nix { };
           }
+        ];
+        corePlugins = [
+          "backlink"
+          "bookmarks"
+          "canvas"
+          "command-palette"
+          "daily-notes"
+          "editor-status"
+          "file-explorer"
+          "file-recovery"
+          "global-search"
+          "graph"
+          "markdown-importer"
+          "note-composer"
+          "outgoing-link"
+          "outline"
+          "page-preview"
+          "properties"
+          "random-note"
+          "slash-command"
+          "slides"
+          "switcher"
+          "tag-pane"
+          "templates"
+          "word-count"
+          "workspaces"
+          "zk-prefixer"
         ];
         communityPlugins = [
           {
