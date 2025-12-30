0fcd07ab8ac50e5b40d631d72396c35a55b01bfb
Tobias Fuchs <tobias.fuchs@tfox.tech>
2025-12-30 21:15:24 +0100

    add fullscreen toggle for hypr

--- modules/home/hypr.nix
+++ modules/home/hypr.nix
@@ -1,25 +1,23 @@
-{ pkgs, dms, ... }:
-
-let
+{
+  pkgs,
+  dms,
+  ...
+}: let
   workspaces = builtins.concatLists (
     builtins.genList (
-      x:
-      let
-        ws =
-          let
-            c = (x + 1) / 10;
-          in
+      x: let
+        ws = let
+          c = (x + 1) / 10;
+        in
           builtins.toString (x + 1 - (c * 10));
-      in
-      [
+      in [
         "$mod, ${ws}, workspace, ${toString (x + 1)}"
         "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
       ]
-    ) 10
+    )
+    10
   );
-in
-{
-
+in {
   imports = [
     dms.homeModules.dankMaterialShell.default
   ];
@@ -104,19 +102,21 @@ in
         allow_tearing = true;
         layout = "master";
       };
-      bind = [
-        "$mod SHIFT, E, exit,"
-        "$mod SHIFT, Q, killactive,"
-        "$mod, V, exec, cliphist list | wofi --dmenu --pre-display-cmd \"echo '%s' | cut -f 2\" | cliphist decode | wl-copy"
-        "$mod, Return, exec, $terminal"
-        "$mod, Space, togglefloating,"
-        "$mod, D, exec, $menu"
-        "$mod, l, movefocus, l"
-        "$mod, h, movefocus, r"
-        "$mod, k, movefocus, u"
-        "$mod, j, movefocus, d"
-      ]
-      ++ workspaces;
+      bind =
+        [
+          "$mod SHIFT, E, exit,"
+          "$mod SHIFT, Q, killactive,"
+          "$mod, V, exec, cliphist list | wofi --dmenu --pre-display-cmd \"echo '%s' | cut -f 2\" | cliphist decode | wl-copy"
+          "$mod, Return, exec, $terminal"
+          "$mod, Space, togglefloating,"
+          "$mod, D, exec, $menu"
+          "$mod, l, movefocus, l"
+          "$mod, h, movefocus, r"
+          "$mod, k, movefocus, u"
+          "$mod, j, movefocus, d"
+          "$mod, f, fullscreen,"
+        ]
+        ++ workspaces;
       bindm = [
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
