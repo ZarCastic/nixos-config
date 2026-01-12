5a562a8c54561f6659b690f88478fce10b730979
Tobias Fuchs <tobias.fuchs@tfox.tech>
2026-01-12 13:18:38 +0100

    purge nvf

--- flake.nix
+++ flake.nix
@@ -23,16 +23,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nvf = {
-      url = "github:NotAShelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # nixvim = {
-    #   url = "github:nix-community/nixvim";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,8 +32,6 @@
   outputs = {
     nixpkgs,
     home-manager,
-    nvf,
-    # nixvim,
     quickshell,
     dms,
     stylix,
@@ -61,15 +49,9 @@
         ./hosts/tobi-tower/configuration.nix
         ./modules/nixos
         stylix.nixosModules.stylix
-        nvf.nixosModules.default
-        ./modules/nvf
-        # nixvim.homeModules.nixvim
         home-manager.nixosModules.home-manager
         {
           home-manager = {
-            # extraSpecialArgs = {
-            #   inherit dms;
-            # };
             useGlobalPkgs = true;
             useUserPackages = true;
             users.tobi = import ./hosts/tobi-tower/home.nix;
