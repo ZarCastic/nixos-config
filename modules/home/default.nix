122ad9a9144319b0ebecb3d13a23a24686018e56
Tobias Fuchs <tobias.fuchs@tfox.tech>
2026-01-13 20:43:21 +0100

    Add terminal home config for ghostty

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -13,7 +13,8 @@
     ./hypr.nix
     ./obsidian
     ./zsh
-    ./tmux
+    ./tmux.nix
+    ./terminal.nix
   ];
 
   programs = {
@@ -32,17 +33,6 @@
     fd.enable = true;
     fzf.enable = true;
     gcc.enable = true;
-    ghostty = {
-      enable = true;
-      package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      enableFishIntegration = false;
-      settings = {
-        font-family = "Monaspace Xenon Var";
-        font-feature = "liga,ss01,ss02,ss03,ss04,ss05,ss06,ss07,ss08,ss09,ss10";
-      };
-    };
     git = {
       enable = true;
       includes = [
