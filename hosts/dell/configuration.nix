e80aae3ce823068750f8ddd743ea4d0bedf2cec9
Tobias Fuchs <tobias.fuchs@wemolo.com>
2026-01-19 17:50:53 +0100

    add bluetooth and battery for dell

--- hosts/dell/configuration.nix
+++ hosts/dell/configuration.nix
@@ -6,9 +6,12 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/nixos
+    ../../modules/nixos/bluetooth.nix
+    ../../modules/nixos/powermanagement.nix
   ];
 
-  boot.initrd.luks.devices."luks-5d12f2b1-766f-45a6-847c-ad23b23cbedc".device = "/dev/disk/by-uuid/5d12f2b1-766f-45a6-847c-ad23b23cbedc";
+  boot.initrd.luks.devices."luks-5d12f2b1-766f-45a6-847c-ad23b23cbedc".device =
+    "/dev/disk/by-uuid/5d12f2b1-766f-45a6-847c-ad23b23cbedc";
 
   networking.hostName = "${hostname}";
 }
