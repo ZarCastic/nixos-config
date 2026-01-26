736e7b81d5718c1746508f11391e3887ea2df361
Tobias Fuchs <tobias.fuchs@wemolo.com>
2026-01-26 17:54:39 +0100

    add allow session lock restore to hyprland config

--- modules/home/hypr/default.nix
+++ modules/home/hypr/default.nix
@@ -101,6 +101,8 @@ in
         disable_autoreload = false;
         force_default_wallpaper = 0;
         vrr = 1;
+        disable_hyprland_logo = true;
+        allow_session_lock_restore = true;
       };
       "$mod" = "SUPER";
       "$terminal" = "footclient";
